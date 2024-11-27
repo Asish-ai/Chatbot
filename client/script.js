95cb13406b88230659b87ec4dc90b19c3cda8096
Asish-ai <asishbeniya@gmail.com>
2024-11-28 00:51:15 +0530

    updated server

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   try {
-    const response = await fetch('http://localhost:5001/chat', {
+    const response = await fetch('https://lisa-r18m.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
